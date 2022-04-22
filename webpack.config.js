05fba52c4814229af84cc14428d2aff18cd90350
App Academy Student <email@email.com>
2022-04-22 16:42:44 -0400

    setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// webpack.config.js
+var path = require('path');
+
+module.exports = {
+  entry: './frontend/error_404.jsx',
+  output: {
+    filename: './bundle.js',
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts')
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
