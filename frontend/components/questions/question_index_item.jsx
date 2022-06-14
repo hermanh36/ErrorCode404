df66895f83a338884637ffcaa77a47d5313ab9ec
Herman <hjiper2@DESKTOP-EU3DC6S.localdomain>
2022-06-14 16:13:18 -0400

    vote display bugfix on index page

--- frontend/components/questions/question_index_item.jsx
+++ frontend/components/questions/question_index_item.jsx
@@ -8,32 +8,45 @@ class QuestionIndexItem extends React.Component {
   }
 
   componentDidMount() {
+    let counter = 0;
     this.props.fetchVote(this.props.question.id)
-    .then(votes => {
-      if (votes) {
-        this.setState({numVotes:Object.values(votes.votes).length})
+    .then(res => {
+      if (res.votes) {
+        Object.values(res.votes).forEach(vote => {
+          if (vote.votableType === 'Question'){
+             counter +=1;
+          }
+        })
       }
     })
+    .then(() => this.setState({count: counter}))
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.question.id !== this.props.question.id) {
-      this.props.fetchVote(this.props.question.id)
-      .then(votes => {
-        if (votes) {
-          this.setState({numVotes:Object.values(votes.votes).length})
-        }
-      })
-    }
-  }
+  // componentDidUpdate(prevProps) {
+  //   if (prevProps.question.id !== this.props.question.id) {
+  //     this.props.fetchVote(this.props.question.id)
+  //     .then(res => {
+  //       if (res.votes) {
+  //         Object.values(res.votes).forEach(vote => {
+  //           if (vote.votableType === 'Question'){
+  //             this.count +=1;
+  //           }
+  //         })
+  //       }
+  //     })
+  //   }
+  // }
 
 
   render() {
+    if (!this.props.votes) {
+      return null
+    } else {
       return (
         <li>
           <div id='question-item-container'>
             <div id='question-item-votes-answers-container'>
-              <div id='question-item-vote-box'><p>{this.state?.numVotes ? this.state.numVotes : 0} vote</p></div>
+              <div id='question-item-vote-box'><p>{this.state.count} vote</p></div>
               <div id='question-item-answer-box'><span id='question-item-answer'>{this.props.question?.answers ? this.props.question.answers.length : 0} answer</span></div>
             </div>
             <div id='question-main-container'>
@@ -43,7 +56,8 @@ class QuestionIndexItem extends React.Component {
           </div>
         </li>
       )
-    }  
+    }
+  }  
 
 }
 
